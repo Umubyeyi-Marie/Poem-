e3fd14cba89894252d64f18e95e5f9ded5c4a6ff
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2025-04-16 17:09:19 +0200

    second push

--- src/component.js/poem.jsx
+++ src/component.js/poem.jsx
@@ -19,7 +19,7 @@ export default function Poem(){
   };
   const renderPoem = () => {
     if(!isSubmitted){
-        return <em className="flex justify-center items-center"> Fill in the form to reveal your Poem...</em>
+        return <em className="flex justify-center items-center p-3"> Fill in the form to reveal your Poem...</em>
     }
     return(
         <>
@@ -37,11 +37,11 @@ export default function Poem(){
 
         </div>
     </div>
-    <div className="flex justify-center items-center bg-gray-700 pt-5 pb-5 m-8 p-6 max-w-md  mx-auto rounded-xl">
+    <div className="flex justify-center items-center bg-gray-700  p-8 w-96  mx-auto rounded-xl">
         <form action="Submit" onSubmit={handleSubmit}className="flex flex-col  gap-5 ">
-        <input type="text" value={firstName} className="p-2 border rounded-xl w-96" placeholder="First Name" onChange = {(e) => setFirstName(e.target.value)}/>
-        <input type="text" value={lastName} className="p-2 border rounded-xl w-96"  placeholder="Last Name" onChange ={(e) => setLastName(e.target.value)}/>
-        <input  className="p-2 border rounded-xl w-96" type="text" value={adress} placeholder="Adress"  onChange = { (e) => setAdress(e.target.value)}/>
+        <input type="text" value={firstName} className="p-2 border rounded-xl w-80" placeholder="First Name" onChange = {(e) => setFirstName(e.target.value)}/>
+        <input type="text" value={lastName} className="p-2 border rounded-xl w-80"  placeholder="Last Name" onChange ={(e) => setLastName(e.target.value)}/>
+        <input  className="p-2 border rounded-xl w-80" type="text" value={adress} placeholder="Adress"  onChange = { (e) => setAdress(e.target.value)}/>
         <button type="Submit" className="bg-green-300 px-5 py-2 rounded-xl">Submit</button>
         </form>
     </div>
